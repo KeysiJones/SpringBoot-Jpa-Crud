1b55bc93d1b3b506b910685805afa5bb9eab9618
keysijones <keysijonessud@gmail.com>
2020-12-12 20:29:00 -0300

    melhoria no feedback ao usuário

--- src/main/resources/static/js/cadastrar.js
+++ src/main/resources/static/js/cadastrar.js
@@ -1,17 +1,48 @@
 $(document).ready(function() {
 
-    $("#form-cadastrar").submit((evento) => {
 
+
+    $("#form-cadastrar").submit((evento) => {
         evento.preventDefault()
+
+
+        let nameField = document.querySelector("#form-nome").value;
+        let emailField = document.querySelector("#form-email").value;
+        let nameError = '';
+        let emailError = '';
 
         $.ajax({
             method: 'post',
             url: "http://localhost:8080/user/add",
             data: $("#form-cadastrar").serialize(),
-            before: (resposta) => console.log(resposta),
-            complete: (resposta) => console.log(resposta),
-            success: (resposta) => console.log(resposta),
-            error: (resposta) => console.log(resposta)
+            before: () => {
+            },
+            complete: () => {
+
+                console.log(nameField.length, emailField.length)
+                console.log("complete")
+                if(nameField.length >= 5 && emailField.length >= 5){
+                    $("#Cadastrar-Feedback").html("<div class='alert alert-success mt-2 ml-4 mr-4'>Cadastrado com sucesso !</div>");
+                    $("#Cadastrar-Feedback").show()
+                    return
+                } 
+                
+                if(nameField.length < 5) {
+                    nameError = "- O Nome deve ter no mínimo 5 caracteres !<br>"
+                }
+
+                if(emailField.length < 5) {
+                    emailError = "- O Email deve ter no mínimo 5 caracteres !<br>"
+                }
+
+                $("#Cadastrar-Feedback").html(`<div class='alert alert-danger mt-2 ml-4 mr-4'>${emailError + nameError}</div>`);
+                $("#Cadastrar-Feedback").show()
+
+
+            },
+            error: (resposta) => {
+                console.log(resposta);
+            }
         })
     })
         
